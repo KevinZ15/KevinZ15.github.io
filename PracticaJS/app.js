9971598390dfd46f1b746d0ce1b9dde908e63723
Kevin <zamorakevin1578@gmail.com>
2021-03-01 19:39:08 -0600

    Agregando carpeta con practica de JS

---
+++ PracticaJS/app.js
@@ -0,0 +1,9 @@
+let altura = prompt('Digite la altura');
+let base = prompt('Digite la base');
+
+const area = (base,altura) =>{
+    let resultado = base*altura;
+    alert(resultado);
+}
+
+area (base,altura);
